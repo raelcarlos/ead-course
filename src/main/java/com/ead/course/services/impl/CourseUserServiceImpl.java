c1ec810a39b8e0d7cd513e55f07aa75b90acea2e
Israel Carlos <israel.carlos@softplan.com.br>
2022-05-17 21:39:09 -0300

    criado entidade para relacionar os usuarios de um curso

---
+++ src/main/java/com/ead/course/services/impl/CourseUserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.ead.course.services.impl;
+
+import com.ead.course.repositories.CourseUserRepository;
+import com.ead.course.services.CourseUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class CourseUserServiceImpl implements CourseUserService {
+
+    @Autowired
+    private CourseUserRepository courseUserRepository;
+}
